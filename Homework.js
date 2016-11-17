41f231222076be10bf75737f1ca29b5dadfb71be
KyleTranquilli27 <KyleTranquilli27@gmail.com>
2016-11-16 19:46:04 -0500

    Update-1

---
+++ Homework.js
@@ -0,0 +1,34 @@
+
+function changeColor(newColor){
+var elem = document.getElementById("para1");
+elem.style.color = newColor;
+    
+}
+
+function changeColor1(newColor){
+var elem = document.getElementById("Button2");
+elem.style.color = newColor;
+    
+}
+
+function toggle_div_fun(id) {
+      //!! toggle the appearance of the div//!!
+    
+    var divelement = document.getElementById(id);
+    if(divelement.style.display == 'none')
+        divelement.style.display = 'block';
+    else
+        divelement.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+ 
+ 
+
+
